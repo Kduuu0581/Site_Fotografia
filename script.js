534c0dd5baaaa61a4fdcd748f8fb46609aec770a
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2023-11-09 14:42:40 -0300

    add blog 1.0

--- script.js
+++ script.js
@@ -10,3 +10,6 @@ showBtn.addEventListener('click', function(){
         showBtn.innerHTML = '<i class = "fas fa-times"></i>';
     }
 });
+
+/* Lightbox */
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
